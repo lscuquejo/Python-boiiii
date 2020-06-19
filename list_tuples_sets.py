47137c54ddd70a8b6ef61f300eae6a891232c162
lscuquejo <leonardo.s@cuquejo.org>
2020-06-19 15:35:45 +0200

    how to check list valuebles... etc

--- list_tuples_sets.py
+++ list_tuples_sets.py
@@ -1,7 +1,8 @@
 my_variable = "hello"
 
-list_grades = [99, 80, 90, 95, 100]
+list_grades = [99, 80, 90]
 tuple_grades = (77, 80, 90, 100, 105) # immutable
 set_grades = {77, 80, 90, 100} # unique & unnordered
 
-print(sum(grades) / len(grades))
+list_grades.append(100) # add a value to a list
+print(list_grades)
