7fa302b2d5566cd3edd59e49c2478f6863aaf950
lscuquejo <leonardo.s@cuquejo.org>
2020-06-19 15:45:11 +0200

    Set operations

--- list_tuples_sets.py
+++ list_tuples_sets.py
@@ -5,4 +5,13 @@ tuple_grades = (77, 80, 90, 100, 105) # immutable
 set_grades = {77, 80, 90, 100} # unique & unnordered
 
 list_grades.append(100) # add a value to a list
-print(list_grades)
+#print(list_grades)
+
+# Set operations
+
+your_lottery_numbers = {1, 2, 3, 4, 5}
+winning_numbers = {1, 3, 5, 7, 9, 11}
+
+#print(your_lottery_numbers.intersection(winning_numbers))
+
+print(your_lottery_numbers.union(winning_numbers))
