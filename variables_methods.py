b1b99e4816cbb34244592ed59f9c36ef20ac9fee
lscuquejo <leonardo.s@cuquejo.org>
2020-06-19 12:59:44 +0200

    Hello world ! but in python WOOOOOOWWW

---
+++ variables_methods.py
@@ -0,0 +1,13 @@
+a = 5
+b = 10
+my_variable = 56
+my_10_variable = 10
+
+
+string_variable = "hello"
+signle_quotes = 'strings can have single quotes'
+
+print(my_variable)
+print(string_variable)
+print("hello, world!")
+print(123)
