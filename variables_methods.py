f143d1fc27f55beb826496e429cab4307919091d
lscuquejo <leonardo.s@cuquejo.org>
2020-06-19 15:16:43 +0200

    Methods

--- variables_methods.py
+++ variables_methods.py
@@ -1,7 +1,24 @@
-var1 = "hi"
-var2 = "hi"
+a = 5
+b = 10
+my_variable = 56
+my_10_variable = 10
 
-num1 = 4
-num2 = 4
 
-print(num1*num2)
+string_variable = "hello"
+signle_quotes = 'strings can have single quotes'
+
+#print(my_variable)
+#print(string_variable)
+
+
+def my_print_method(my_parameter):
+    print(my_parameter)
+
+
+def my_multuply_method(number_one, number_two):
+    return number_one * number_two
+
+result = my_multiply_method(5, 3)
+#my_print_method(result)
+
+my_print_method(my_multiply_method(5, 3))
